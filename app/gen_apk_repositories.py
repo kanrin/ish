bf6990d2cca11064f2ffc72c461ac7e4c9d1a247
Orz <hanlin@uxin.com>
2026-09-22 16:12:15 +0800

    tmp: 1

--- app/gen_apk_repositories.py
+++ app/gen_apk_repositories.py
@@ -9,14 +9,15 @@ APK_REPOSITORIES = [
     ('v3.19', 'main'),
     ('v3.19', 'community'),
 ]
-ARCH = 'x86' # TODO: support more archs
+# ARCH = 'x86' # TODO: support more archs
+ARCH = 'arm64' # TODO: support more archs
 
 repos_file = []
 for version, repo in APK_REPOSITORIES:
-    with open(f'{os.environ["SRCROOT"]}/deps/aports/{version}/{repo}/{ARCH}/index.txt') as f:
-        index_name = f.read()
-    index_name = trim(index_name, 'APKINDEX-', '.tar.gz\n')
-    repos_file.append(f'http://apk.ish.app/{index_name}/{repo}')
+    # with open(f'{os.environ["SRCROOT"]}/deps/aports/{version}/{repo}/{ARCH}/index.txt') as f:
+    #     index_name = f.read()
+    # index_name = trim(index_name, 'APKINDEX-', '.tar.gz\n')
+    repos_file.append(f'http://mirrors.tuna.tsinghua.edu.cn/alpine/{version}/{repo}')
 
 with open(os.path.join(os.environ['BUILT_PRODUCTS_DIR'], os.environ['CONTENTS_FOLDER_PATH'], 'repositories.txt'), 'w') as f:
     for line in repos_file:
